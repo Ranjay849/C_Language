f7944bc120a7fa796966c57b0336a8b3a96b4140
Ranjay <ranjayku745@gmail.com>
2025-03-01 00:01:19 +0530

    This is Assignment-02

--- Assignment-02/01.c
+++ Assignment-02/01.c
@@ -6,7 +6,7 @@ int main()
 {
     int a, b, c;
     float avg;
-    printf("Enter three integer numbers.....\n");
+    printf("Enter three integer numbers....\n");
     scanf("%d%d%d", &a, &b, &c);
 
     avg = (a+b+c)/3.0;
